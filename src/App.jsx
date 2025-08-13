9d8eea659fed3f47a66e6fca1e3d09b66d8d67b9
automation.andy101 <automation.andy101@gmail.com>
2025-08-13 20:43:53 +0100

    Started adding main translator components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import TranslatorStart from './components/TranslatorStart'
+import TranslatorApp from './components/TranslatorApp'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-gradient-to-l from-[#b6f492] to-[#338b93] flex justify-center items-center'>
-      <div className='w-[90%] max-w-lg h-[70vh] bg-[#2d2d2d] rounded-xl shadow-2xl shadow-gray-800 flex flex-col'>
-        <TranslatorStart />
+    <div className="w-full h-screen bg-gradient-to-l from-[#b6f492] to-[#338b93] flex justify-center items-center">
+      <div className="w-[90%] max-w-lg max-[392px]:h-[90%] sm:h-auto bg-[#2d2d2d] rounded-xl shadow-2xl shadow-gray-800 flex flex-col">
+        {/* <TranslatorStart /> */}
+        <TranslatorApp />
       </div>    
     </div>
   )
